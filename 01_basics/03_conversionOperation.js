1e6f631be77d9de8539f806b4b89eff06a2ec568
Sahil Dhote <136931176+sd0233@users.noreply.github.com>
2024-01-18 21:14:29 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 //let score = null;
 let score = undefined;
-console.log(score)
+// console.log(score)
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(valueInNumber); 
-console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
 
 //"33" => 33
 // "33abs" => NaN(not a number)
@@ -16,3 +16,34 @@ console.log(typeof valueInNumber);
 // "" => false
 // sahil  => true
 
+//################## Operations #####################
+// let (1+1);
+// let(2-1)
+//etc arethematic operation operation 
+
+// console.log(1 + "2")
+// console.log("7 "+ 8 + 8);
+// console.log("1" + 1 + "1");
+
+// console.log(+true);
+// console.log(-"");
+
+// *************** COMPARISION OF DATATYPES ***************
+
+
+//Compression OF data type gives boolian value
+
+// when we compaire same datatype value then it will give natural result 
+// other wise different types gives slightly different results
+
+console.log("2" > 1);
+console.log(2 > "1") ;
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === operator
+
+console.log("2" === 1);// this operators need to be same datatype to check
