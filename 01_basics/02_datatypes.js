37ab87030ab7516cc05f1e6ff459a8de017f8db0
Sahil Dhote <136931176+sd0233@users.noreply.github.com>
2024-01-18 12:23:52 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+// null => standalone value
+//undefined =>
+// symbol=> unique
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //  object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+//let score = null;
+let score = undefined;
+console.log(score)
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber); 
+console.log(typeof valueInNumber);
+
+//"33" => 33
+// "33abs" => NaN(not a number)
+// true => 1: false =>0
+
+// 1 = > true 
+// "" => false
+// sahil  => true
+
